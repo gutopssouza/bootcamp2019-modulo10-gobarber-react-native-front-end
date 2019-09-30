2164629f95351d3c53809295ba0e9d5c2d048bee
Augusto <gutopssouza@gmail.com>
2019-09-30 18:17:45 -0300

    :construction: Dicas de acessibilidade :construction: Implement input fields ref to redirect from one input to another :construction: Implement navigation from sign up page to sign in page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image } from 'react-native';
 
 import logo from '~/assets/logo.png';
@@ -15,6 +15,9 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
   return (
     <Background>
       <Container>
@@ -27,17 +30,22 @@ export default function SignIn({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Digite sua senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
         </Form>
 
         <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
 
-        <SignLink onPress={() => navigation.navigate('SignUp ')}>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
           <SignLinkText>Criar conta gratuita</SignLinkText>
         </SignLink>
       </Container>
