ca6bbbb52ef3f1880de2068d84ed1c14232a9388
Augusto <gutopssouza@gmail.com>
2019-10-01 16:23:33 -0300

    :construction: Listagem de agendamentos :construction: Create appointment component :construction: Style dashboard page :construction: Style appointment component

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-// import { Container } from './styles';
+
+import { Container } from './styles';
 
 import Background from '~/components/Background';
 
 export default function Profile() {
-  return <Background />;
+  return (
+    <Background>
+      <Container></Container>
+    </Background>
+  );
 }
 Profile.navigationOptions = {
   tabBarLabel: 'Meu perfil',
