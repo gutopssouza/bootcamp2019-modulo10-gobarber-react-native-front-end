846466426b20f26ce36127b7c3bbca0b969b2191
Augusto <gutopssouza@gmail.com>
2019-10-04 12:35:51 -0300

    :rocket: Replace DatePickerAndroid and DatePicker IOS for new DateTimePicker from react-native :construction: Remove src/components/DateInput/index.android.js and  src/components/DateInput/index.ios.js

---
+++ src/components/DateInput/index.js
@@ -0,0 +1,55 @@
+import React, { useState, useMemo } from 'react';
+import { Platform } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, DateButton, DateText, Picker } from './styles';
+
+export default function DateInput({ date, onChange }) {
+  const [opened, setOpened] = useState(false);
+
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
+
+  function setDate(event, date2) {
+    if (date2 !== undefined) {
+      onChange(date2);
+    }
+  }
+
+  return (
+    <Container>
+      <DateButton onPress={() => setOpened(!opened)}>
+        <Icon name="event" color="#FFF" size={20} />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+
+      {opened &&
+        (Platform.OS === 'ios' ? (
+          <Picker>
+            <DateTimePicker
+              value={date}
+              onChange={onChange}
+              minimumDate={new Date()}
+              minuteInterval={60}
+              locale="pt"
+              mode="date"
+            />
+          </Picker>
+        ) : (
+          <DateTimePicker
+            value={date}
+            onChange={setDate}
+            minimumDate={new Date()}
+            minuteInterval={60}
+            locale="pt"
+            mode="date"
+            display="spinner"
+          />
+        ))}
+    </Container>
+  );
+}
